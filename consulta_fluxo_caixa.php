466ca4bee6097ea8ba6bf464ce62422708b39dce
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-06-20 11:22:55 -0300

    20/06

--- consulta_fluxo_caixa.php
+++ consulta_fluxo_caixa.php
@@ -2,7 +2,7 @@
 include("conexao.php");
 $consulta = $_POST['consulta'];
 
-if($consulta == 'st') {
+if($consulta == 'te') {
     $sql ="select sum(valor) valor from fluxo_caixa where tipo = 'entrada'";
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result);
@@ -12,7 +12,7 @@ if($consulta == 'st') {
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result);
     echo "Aqui esta o valor total de saidas do banco ".$row['valor']."<br>";
-} else if($consulta == 'te'){
+} else if($consulta == 'st'){
     $sql ="select sum(case when tipo = 'entrada' then valor else 0 end) - 
     sum(case when tipo = 'saida' then valor else 0 end) as valor from fluxo_caixa";
     $result = mysqli_query($con,$sql);
